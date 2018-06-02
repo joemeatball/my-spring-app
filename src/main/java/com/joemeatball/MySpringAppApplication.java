2548964824a09bb0f32dbdaeec91aa572d25144d
joemeatball <michaelburns1@gmail.com>
2018-06-02 12:33:05 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/joemeatball/MySpringAppApplication.java
@@ -0,0 +1,12 @@
+package com.joemeatball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringAppApplication.class, args);
+	}
+}
